348ebbcab619769bdb1f311f4cc1cd45a9931c28
Noah Kurinsky <nkurinsky91@gmail.com>
2013-12-03 23:08:32 -0500

    Some Basic Functional Simulations Done

--- Adder/XOR_Static/functional/verilog.v
+++ Adder/XOR_Static/functional/verilog.v
@@ -11,9 +11,9 @@ module XOR_Static ( Y, A, B, B_ );
   wire A_;
   wire L,R;
 
-  not #(1) n1(A_,A);
-  and #(3) a1(L,A,B);
-  and #(3) a2(R,A_,B_);
+  not #(1.333,0.666) n1(A_,A);
+  and #(3) a1(L,A,B_);
+  and #(3) a2(R,A_,B);
   or (Y,L,R);
 
 endmodule
